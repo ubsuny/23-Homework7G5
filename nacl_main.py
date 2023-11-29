f8cd665d0918efb4d8c2cf09aa6d51af86090d61
sharmistharanit <143737948+sharmistharanit@users.noreply.github.com>
2023-11-29 12:51:59 -0500

    Update nacl_main.py

--- nacl_main.py
+++ nacl_main.py
@@ -3,3 +3,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import itertools
 import scipy.optimize
+
+ke2 = 197 / 137 # eV-nm   Coulomb force charge
+alpha = 1.09e3  # eV      parameter of model
+rho = 0.0321    # nm      parameter of model
+b = 1.0         # eV      regular
+c = 0.01        # nm
